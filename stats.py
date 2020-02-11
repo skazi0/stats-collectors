c06c52c0267ac86c26877146a2a605bff19826f3
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2020-02-11 13:31:04 +0100

    added support for local collector config file

--- stats.py
+++ stats.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import requests
 
@@ -7,15 +8,16 @@ logging.basicConfig()
 logger = logging.getLogger('stats')
 #logger.setLevel(logging.DEBUG)
 
-# load config
-try:
-    with open('/etc/default/stats-collectors', 'r') as f:
-        for line in f:
-            parts = line.split('=')
-            if parts[0] == 'URL':
-                stats_url = parts[1].strip()
-except:
-    pass
+# load config from global and local files
+for conf in ['/etc/default/stats-collectors', os.path.join(os.environ['HOME'], '.stats-collectors')]:
+    try:
+        with open(conf, 'r') as f:
+            for line in f:
+                parts = line.split('=')
+                if parts[0] == 'URL':
+                    stats_url = parts[1].strip()
+    except:
+        pass
 
 def write_points(table, samples):
     for point in samples:
