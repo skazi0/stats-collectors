9181701f0c6ac02b45b35a22df2fdd780a53cdab
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2021-09-27 16:19:20 +0200

    Updated airq collector to new API

--- download-airq-stats.py
+++ download-airq-stats.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# api docs: http://powietrze.gios.gov.pl/pjp/content/api
 # -*- coding: utf8 -*-
 import os
 import sys
@@ -10,8 +11,7 @@ from datetime import datetime
 
 import stats
 
-#url = 'https://demo.dacsystem.pl/pobierz-dane-mapy'
-url = 'http://air.wroclaw.pios.gov.pl/pobierz-dane-mapy'
+url = 'http://api.gios.gov.pl/pjp-api/rest/'
 
 table = 'air_quality'
 
@@ -22,30 +22,30 @@ index_values = {
     u'Dostateczny': 3.0,
     u'Zły': 2.0,
     u'Bardzo zły': 1.0,
-    u'Brak pomiaru': 0.0,
+    u'Brak indeksu': 0.0,
 }
 
 logging.basicConfig()
 logger = logging.getLogger('airq')
 #logger.setLevel(logging.DEBUG)
 
+# load norms from file (these are based on old API response)
+norms = {}
+with open(os.path.join(os.path.dirname(__file__), 'norms.json')) as f:
+    norms = json.load(f)
+
 session = requests.session()
 
 try:
-    r = session.get(url)
+    r = session.get(url + 'station/findAll')
     json = r.json()
 except:
-    logger.error('error fetching air quality data')
+    logger.error('error fetching stations list')
     sys.exit(1)
 
-if json.get('error', False):
-    logger.error('error returned from server: %s' % json['msg'])
-    sys.exit(2)
-
-#json = json.loads(open('pobierz-dane-mapy').read())
-
-cps = json['compounds']
-norms = json['norms']
+#if json.get('error', False):
+#    logger.error('error returned from server: %s' % json['msg'])
+#    sys.exit(2)
 
 def clean_text(text):
     text = text.replace('&micro;', 'u')
@@ -62,36 +62,51 @@ def find_index(value, norm):
     return ''
 
 samples = []
-for station in json['stations']:
-    meta = station['params']
-    if not meta['name'].startswith(u'Wrocław'):
+for station in json:
+    if not station['stationName'].startswith(u'Wrocław'):
         continue
-#    logger.debug(meta)
-    if not station['values']:
-        logger.warning('no values for station %s' % meta['name'])
+    stationID = station['id']
+    try:
+        r = session.get(url + 'station/sensors/%d' % stationID)
+        sensors = r.json()
+    except:
+        logger.error('error fetching sensors for station ' + station['stationName'])
         continue
-    for key, value in station['values'].items():
+
+    for sensor in sensors:
+        key = sensor['param']['paramCode'].lower()
 #        logger.debug(key)
 #        logger.debug(norms[key])
 #        logger.debug(find_index(value['value'], norms[key]))
 
-        index_label = find_index(value['value'], norms[key])
+        try:
+            r = session.get(url + 'data/getData/%d' % sensor['id'])
+            data = r.json()
+        except:
+            logger.error('error fetching data for sensor %d', sensor['id'])
+            continue
 
-        sample = {
-            'time': value['ts'],
-            'value': max(-1.0, 1.0 * value['value']),
-            'qindexnum': index_values[index_label],
-            'qindex': index_label,
-            'label': clean_text(cps[value['cp']]['long']),
-            'unit': clean_text(cps[value['cp']]['unit']),
-            'tags': {
-#                'plId': meta['plId'],
-#                'euId': meta['euId'],
-                'station': meta['name'],
-                'type': clean_text(cps[value['cp']]['code'])
+        # take max 2 last values
+        for v in data['values'][:2]:
+            if v['value'] is None:
+                continue
+            index_label = find_index(v['value'], norms[key])
+            ts = datetime.strptime(v['date'], '%Y-%m-%d %H:%M:%S').timestamp()
+            sample = {
+                'time': int(ts),
+                'value': max(-1, v['value']),
+                'qindexnum': index_values[index_label],
+                'qindex': index_label,
+                'label': sensor['param']['paramName'],
+#                'unit': clean_text(cps[value['cp']]['unit']),
+                'tags': {
+#                    'plId': meta['plId'],
+#                    'euId': meta['euId'],
+                    'station': station['stationName'],
+                    'type': sensor['param']['paramCode'],
+                }
             }
-        }
-        samples.append(sample)
-        logger.debug(sample)
+            samples.append(sample)
+            logger.debug(sample)
 
 stats.write_points(table, samples)
